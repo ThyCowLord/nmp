d20df985ee07da2f30444739b99745ba5f182e1a
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-26 19:04:11 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='NMP',
+      version='1.0',
+      description='Truly random numbers, easily.',
+      author='ThyCowLord',
+      url='https://github.com/thycowlord/nmp',
+      packages=['nmp'],
+     )
